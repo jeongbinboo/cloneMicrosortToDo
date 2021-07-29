12a0e35063c1634a1d0ffba7775cbad28fdfb662
jeongbinboo <jrkasey3461@naver.com>
2021-07-30 01:01:25 +0900

    commit

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,3 +1,4 @@
+// set event of todo input
 const todoInput = document.querySelector("#todoInput");
 const icon = document.querySelector("#icon");
 todoInput.addEventListener("focus", () => {
@@ -8,24 +9,69 @@ todoInput.addEventListener("focusout", () => {
   icon.innerHTML = '<i class="fas fa-plus"></i>';
 });
 
+// when input is submitted
 const form = document.querySelector("form");
-form.addEventListener("submit", addTodo);
+form.addEventListener("submit", whenSubmitted);
 
-function addTodo(event) {
+const todoArr = getTodosFromLocal();
+
+function getTodosFromLocal() {
+  let todosFromLocal = localStorage.getItem("todos");
+  if (todosFromLocal) {
+    todosFromLocal = todosFromLocal.split(",");
+    return todosFromLocal;
+  }
+  return [];
+}
+
+function whenSubmitted(event) {
   event.preventDefault();
   const input = todoInput;
-  for (let i = 1; i < 16; i++) {
+  for (let i = 0; i < 15; i++) {
     const todo = document.querySelector(`#todo${i}`);
     if (todo.innerText === "") {
-      todo.innerHTML = `<button><i class="far fa-circle"></i></button>${input.value}`;
-      todo.classList.add("todoEntered");
-      /*todo.addEventListener("click", () => {
-        alert("clicked!");
-      });*/ //나중에 토글 적용!
-      if (localStorage.getItem("todos")) {
-      }
+      insertTodo(i, input.value);
+      todoArr.push(input.value);
+      localStorage.setItem("todos", todoArr);
       break;
     }
   }
   input.value = "";
+}
+
+//first setting
+const rightBanner = document.querySelector("#popUpTodo");
+
+function setTodo() {
+  for (let i = 0; i < todoArr.length; i++) {
+    insertTodo(i, todoArr[i]);
+  }
+  rightBanner.style.display = "none";
+}
+
+function insertTodo(num, text) {
+  const todo = document.querySelector(`#todo${num}`);
+  todo.innerHTML = `<button><i class="far fa-circle"></i></button>${text}`;
+  todo.classList.add("todoEntered");
+  todo.addEventListener("click", popUp);
+}
+
+function popUp(event) {
+  rightBanner.style.display = "block";
+  rightBanner.innerText = event.target.innerText;
+}
+
+setTodo();
+
+//today setting
+setInterval(setTime, 1000);
+setTime();
+function setTime() {
+  const dayArr = ["", "월", "화", "수", "목", "금", "토", "일"];
+  const today = document.querySelector("#date");
+  const now = new Date();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  const day = now.getDay();
+  today.innerText = `${month}월 ${date}일, ${dayArr[day]}요일`;
 }
